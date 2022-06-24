1452735ca26710bb95e7c37e7b4795ba1517faf1
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-06-24 12:21:59 -0700

    2022/06/24 12:30

---
+++ DicisionTree.py
@@ -0,0 +1,8 @@
+def uniquecounts(rows):
+    results = {}
+    for row in rows:
+        r = row[len(row)-1]
+        if r not in results:
+            results[r] = 0
+        results[r] += 1
+    return results
