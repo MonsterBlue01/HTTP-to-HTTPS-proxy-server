51f02114dbe7e1e3c73053604daa98feacf65381
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-06-28 13:22:55 -0700

    2022/06/28 13:30

--- DicisionTree.py
+++ DicisionTree.py
@@ -1,3 +1,5 @@
+from math import log
+
 def uniquecounts(rows):
     results = {}
     for row in rows:
@@ -6,3 +8,14 @@ def uniquecounts(rows):
             results[r] = 0
         results[r] += 1
     return results
+
+def entropy(rows):
+    log2 = lambda x:log(x)/log(2)
+    results = uniquecounts(rows)
+    ent = 0.0
+    for r in results.keys():
+        p = float(results[r])/len(rows)
+        ent = ent - p*log2(p)
+    return ent
+
+# https://zhuanlan.zhihu.com/p/20794583
