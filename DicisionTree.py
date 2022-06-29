74988fdd3683b324536c837059a6820a65099aa3
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-06-29 13:22:03 -0700

    2022/06/29 13:30

--- DicisionTree.py
+++ DicisionTree.py
@@ -1,5 +1,13 @@
 from math import log
 
+class decisionnode:
+    def __init__(self,col = -1,value = None, results = None, tb = None,fb = None):
+        self.col = col
+        self.value = value
+        self.results = results
+        self.tb = tb
+        self.fb = fb
+
 def uniquecounts(rows):
     results = {}
     for row in rows:
@@ -17,5 +25,17 @@ def entropy(rows):
         p = float(results[r])/len(rows)
         ent = ent - p*log2(p)
     return ent
+
+def giniimpurity(rows):
+    total = len(rows)
+    counts = uniquecounts(rows)
+    imp = 0
+    for k1 in counts:
+        p1 = float(counts[k1])/total
+        for k2 in counts:
+            if k1 == k2: continue
+            p2 = float(counts[k2])/total
+            imp += p1*p2
+    return imp
 
 # https://zhuanlan.zhihu.com/p/20794583
