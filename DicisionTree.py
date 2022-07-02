1844d812d0b8d61f067e04c6101b03c71ddb7a79
Dongjing Wang <wdsrs020808@gmail.com>
2022-07-02 02:46:40 +0000

    2022/07/01 20:00

--- DicisionTree.py
+++ DicisionTree.py
@@ -38,4 +38,23 @@ def giniimpurity(rows):
             imp += p1*p2
     return imp
 
+def giniimpurity_2(rows):
+    total = len(rows)
+    counts = uniquecounts(rows)
+    imp = 0
+    for k1 in counts.keys():
+        p1 = float(counts[k1])/total
+        imp+= p1*(1-p1)
+    return imp
+
+def divideset(rows,column,value):
+    split_function = None
+    if isinstance(value,int) or isinstance(value,float):
+        split_function = lambda row:row[column] >= value
+    else:
+        split_function = lambda row:row[column]==value
+    set1 = [row for row in rows if split_function(row)]
+    set2 = [row for row in rows if not split_function(row)]
+    return(set1,set2)
+
 # https://zhuanlan.zhihu.com/p/20794583
